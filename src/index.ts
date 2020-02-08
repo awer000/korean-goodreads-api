469a8ce0e2de31ce800ab7066e7831c159b9c5dc
awer000 <awer000@naver.com>
2020-02-08 16:35:00 +0900

    #Day 8 - 타입스크립트 서버 만들기 프로젝트 세팅

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/', (request: Request, response: Response, next: NextFunction) => {
+  response.send('hello');
+});
+
+app.listen(3000, () => {
+  console.log('start')
+})
